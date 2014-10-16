53258db1bc254903b728ea27e35206c14d0c29ec
tompascall <tompascall@gmail.com>
2014-10-16 23:07:41 +0200

    add Vector constructor

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+
+function Vector(x, y){
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(other){
+  return new Vector(this.x + other.x, this.y + other.y);
+};
