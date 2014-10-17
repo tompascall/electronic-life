116d111e108b55e2d78992f3732b77c7fc2407ad
tompascall <tompascall@gmail.com>
2014-10-17 22:36:16 +0200

    add World function

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ var plan = ["############################",
             "#    #                     #",
             "############################"];
 
+var directions = {
+  "n": new Vector(0, -1),
+  "ne": new Vector(1, -1),
+  "e": new Vector(1, 0),
+  "se": new Vector(1, 1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
 function Vector(x, y){
   this.x = x;
   this.y = y;
@@ -21,3 +32,56 @@ function Vector(x, y){
 Vector.prototype.plus = function(other){
   return new Vector(this.x + other.x, this.y + other.y);
 };
+
+function Grid(width, height){
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+
+function randomElement(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+Grid.prototype.isInside = function(vector){
+  return vector.x >= 0 && vector.x <= this.width &&
+    vector.y >= 0 && vector.y <= this.height;
+};
+
+Grid.prototype.get = function(vector){
+  return this.space[vector.x + vector.y * this.width];
+};
+
+Grid.prototype.set = function(vector, value){
+  this.space[vector.x + vector.y * this.width] = value;
+};
+
+function BouncingCritter(){
+  this.direction = randomElement(Object.keys(directions));
+}
+
+BouncingCritter.prototype.act = function(view){
+  if (view.look(this.direction) != " "){
+    this.direction = view.find(" ") || "s";
+  };
+  return {type: "move", direction: this.direction};
+};
+
+function elementFromChar(legend, ch){
+  if (ch === " ") return null;
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function World(map, legend){
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function(line, y){
+    for (var x = 0; x < line.length; x++){
+      grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+    };
+  });
+}
